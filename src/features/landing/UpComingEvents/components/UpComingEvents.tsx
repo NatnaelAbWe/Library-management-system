55f4e5363b8d599e295f6680f2bf00dee6f1a88c
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-02-20 00:40:47 +0300

    feat: upcoming events functionalitly

---
+++ src/features/landing/UpComingEvents/components/UpComingEvents.tsx
@@ -0,0 +1,58 @@
+import { AutoAwesome } from "@mui/icons-material";
+import "./UpComingEvent.css";
+
+export const UpcomingEvents: React.FC = () => {
+  return (
+    <div className="upcoming-events">
+      <div className="upcoming-events-header-group">
+        <AutoAwesome
+          sx={{
+            fontSize: "2.25rem",
+            color: "#3626A7",
+          }}
+        />
+        <h2>Upcoming Events</h2>
+        <AutoAwesome
+          sx={{
+            fontSize: "2.25rem",
+            color: "#3626A7",
+          }}
+        />
+      </div>
+
+      <h3>This Summer</h3>
+
+      <h4>Tuesday's: 10:00 AM - Noon</h4>
+      <ul className="upcoming-events-event">
+        <li>
+          <p>Who: Children to 6th grade</p>
+        </li>
+        <li>
+          <p>Activities: Logic Puzzles, Scratch Programming</p>
+        </li>
+      </ul>
+
+      <h4>Wednesday's: 10:00 AM - Noon</h4>
+      <ul className="upcoming-events-event">
+        <li>
+          <p>Who: Adults (19+)</p>
+        </li>
+        <li>
+          <p>
+            Activities: Craft and Sip - Come enjoy a nice beverage and craft
+          </p>
+        </li>
+      </ul>
+
+      <h4>Thursday's: 10:00 AM - Noon</h4>
+      <ul className="upcoming-events-event">
+        <li>
+          <p>Who: Teens (7th to 12th grade)</p>
+        </li>
+        <li>
+          <p>Activities: Web Programming course</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
