1cf47da781633469f2dd8e3cb871b12bee31b94b
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-02-24 16:47:05 +0300

    feat: catalog search feature added

---
+++ src/features/catalog/components/CatalogSearch/CtalogSearch.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import "./CatalogSearch.css";
+import { AppDispatch, RootState } from "../../../../reducx/ReducxStrore";
+import { queryBooks } from "../../../../reducx/slices/BookSlices";
+import { BookCard } from "../../../book";
+
+export const CatalogSearch: React.FC = () => {
+  const bookState = useSelector((state: RootState) => state.book);
+  const dispatch: AppDispatch = useDispatch();
+
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(queryBooks(location.search));
+  }, [location.search]);
+
+  return (
+    <div className="catalog-search">
+      <div className="catalog-search-advanced-search-section">
+        {/* Advanced search inputs would go here */}
+      </div>
+      {!bookState.loading ? (
+        <>
+          <h2>
+            Displaying {bookState.pagingInformation?.pageCount} books out of{" "}
+            {bookState.pagingInformation?.totalCount}
+          </h2>
+          <div className="catalog-search-item-area">
+            {bookState.books.map((book) => (
+              <BookCard key={book.barcode} book={book} />
+            ))}
+          </div>
+          <div className="catalog-search-pages"></div>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
--- src/reducx/slices/BookSlices.ts
+++ src/reducx/slices/BookSlices.ts
@@ -1,17 +1,20 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import type { Book } from "../../models/Book";
+import type { PageInfo } from "../../models/page";
 
 interface BookSliceState {
   loading: boolean;
   error: boolean;
   books: Book[];
+  pagingInformation: PageInfo | null;
 }
 
 const initialState: BookSliceState = {
   loading: true,
   error: false,
   books: [],
+  pagingInformation: null,
 };
 
 // 1. Create the Thunk to fetch books
@@ -24,6 +27,18 @@ export const fetchAllBooks = createAsyncThunk(
       return res.data; // This returns { message: string, count: number, books: Book[] }
     } catch (e) {
       console.log("api error", e);
+      return thunkAPI.rejectWithValue(e);
+    }
+  },
+);
+
+export const queryBooks = createAsyncThunk(
+  "book/query",
+  async (payload: string, thunkAPI) => {
+    try {
+      let req = await axios.get(`http://localhost:8000/book/query${payload}`);
+      return req.data.page;
+    } catch (e) {
       return thunkAPI.rejectWithValue(e);
     }
   },
@@ -45,6 +60,26 @@ export const BookSlice = createSlice({
       state.error = false;
     });
     builder.addCase(fetchAllBooks.rejected, (state) => {
+      state.loading = false;
+      state.error = true;
+    });
+    builder.addCase(queryBooks.pending, (state) => {
+      state.books = [];
+      state.loading = true;
+      state.error = false;
+    });
+    builder.addCase(queryBooks.fulfilled, (state, action) => {
+      state.books = action.payload.items;
+      state.pagingInformation = {
+        totalCount: action.payload.totalCount,
+        currentPage: action.payload.currentPage,
+        totalPage: action.payload.totalPages,
+        limit: action.payload.limit,
+        pageCount: action.payload.pageCount,
+      };
+      state.loading = false;
+    });
+    builder.addCase(queryBooks.rejected, (state) => {
       state.loading = false;
       state.error = true;
     });
