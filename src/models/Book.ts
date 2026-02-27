31346d6c7790de0aa520038a5b1768b777571678
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-02-27 22:54:18 +0300

    feat: loan record model function

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,3 +1,6 @@
+import type { LoanRecord } from "./LoanRecord";
+import type { User } from "./user";
+
 export type Book = {
   _id: string;
   barcode: string;
@@ -10,5 +13,16 @@ export type Book = {
   publisher: string;
   pages: number;
   genre: string;
-  records: [];
+  records: LoanRecord[];
+};
+
+export type CheckoutBookPayload = {
+  book: Book;
+  libraryCard: string;
+  employee: User;
+};
+
+export type CheckinBookPayload = {
+  book: Book;
+  employee: User;
 };
